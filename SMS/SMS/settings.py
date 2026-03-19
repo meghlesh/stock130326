ab49544978dbdc5c4a3d844e81ed6b3453097c8d
meghlesh <meghachaudhari24@gmail.com>
2026-03-19 20:18:55 +0530

    Update settings.py

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'SMS.urls'
-
+Print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
